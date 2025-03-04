4b51fd1097ae8639dc09879f3ba1df341bf9ba86
Nazir⚡ <nazir99iq@gmail.com>
2025-03-04 15:06:12 +0800

    Create font.js

---
+++ src/api/tools/font.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+let { JSDOM } = require('jsdom');
+
+module.exports = function(app) {
+async function stylizeText(text) {
+    let res = await fetch('http://qaz.wtf/u/convert.cgi?text=' + encodeURIComponent(text))
+    let html = await res.text()
+    let dom = new JSDOM(html)
+    let table = dom.window.document.querySelector('table').children[0].children
+    let obj = {}
+    for (let tr of table) {
+      let name = tr.querySelector('.aname').innerHTML
+      let content = tr.children[1].textContent.replace(/^\n/, '').replace(/\n$/, '')
+      obj[name + (obj[name] ? ' Reversed' : '')] = content
+    }
+    return obj
+}
+app.get('/tools/font', async (req, res) => {
+        try {
+            const { text } = req.query;
+            if (!text) {
+                return res.status(400).json({ status: false, error: 'Text is required' });
+            }
+            const { result } = await stylizeText(text);
+            res.status(200).json({
+                status: true,
+                result
+            });
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    });
+};
